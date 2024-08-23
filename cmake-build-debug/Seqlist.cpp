038c6a548cd42146c1aec92e820cb28075e84a05
ukpkmkk <657714340@qq.com>
2024-08-23 17:05:51 +0800

    seqlist delete element

--- cmake-build-debug/Seqlist.cpp
+++ cmake-build-debug/Seqlist.cpp
@@ -1,8 +1,8 @@
 
 
 #include "Seqlist.h"
-/***********************线性表数据结构******************************************/
-
+/*****线性表数据结构*****/
+/***順序表的定義 構造函數***/
 template <typename DataType>
 Seqlist<DataType>::Seqlist(int size)
 {
@@ -12,8 +12,44 @@ Seqlist<DataType>::Seqlist(int size)
         elements = new DataType[maxSize];//分配内存大小
       }
 }
+
+/***析構函數***/
 template <typename DataType>
 Seqlist<DataType>::~Seqlist()
 {
   delete[] elements;
+}
+
+/***順序表的插入操作***/
+template <typename DataType>bool Seqlist<DataType>::inserElement(DataType data)
+{
+    int currentIndex = length ;  //記錄新元素的插入位置
+    if(length >= maxSize)
+    {
+        return false;           //順序表已滿，返回false，插入不成功
+    }
+    else
+    {
+        elements[currentIndex] =data ; //將新元素插入順序表表尾
+        length++;                   //順序表有效長度+1
+        return true;
+    }
+}
+
+/***順序表的刪除操作***/
+template <typename Datatype>
+bool Seqlist<Datatype>::deleteElement(int location)
+{
+    if(location >= length || location < 0)
+    {
+        return false;
+    }
+    else
+    {
+        for (int i = location; i < length ; i++) {
+            elements[i]=elements[i+1];
+        }
+        length--;
+        return true;
+    }
 }
