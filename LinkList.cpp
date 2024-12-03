fa77c089d47108330e6f3a5a3b9f3704e336f517
ukpkmkk <657714340@qq.com>
2024-12-03 18:41:42 +0800

    linklist

---
+++ LinkList.cpp
@@ -0,0 +1,14 @@
+//
+// Created by Administrator on 24-12-3.
+//
+
+#include "LinkList.h"
+template <typyname DataType> class LinkList {
+public:
+    LinkList() {
+        head= new ListNode();
+    }
+    LinkList(ListNode *node) {
+        head=node;
+    }
+};
---
+++ LinkList.h
@@ -0,0 +1,16 @@
+//
+// Created by Administrator on 24-12-3.
+//
+
+#ifndef LINKLIST_H
+#define LINKLIST_H
+
+
+
+class LinkList {
+
+};
+
+
+
+#endif //LINKLIST_H
