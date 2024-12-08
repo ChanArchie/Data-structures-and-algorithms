82bbaa67e8af0c411dbf3a5cc8114cfea5fec1f9
ukpkmkk <657714340@qq.com>
2024-12-08 10:21:34 +0800

    linklist

--- ListNode.cpp
+++ ListNode.cpp
@@ -1,3 +1,23 @@
+#include "ListNode.h"
+
+#include <bits/ranges_base.h>
+#include <bits/range_access.h>
 //
 // Created by Administrator on 24-12-8.
 //
+
+template<typename DataType>
+ListNode<DataType>::ListNode(DataType data, ListNode<DataType> *nextNode) {
+    this->data = data;
+    this->next = nextNode;
+}
+
+template<typename DataType>
+ListNode<DataType> ListNode<DataType>::getNext() {
+    return this->next;
+}
+
+template<typename DataType>
+DataType ListNode<DataType>::getData() {
+    return data;
+}
