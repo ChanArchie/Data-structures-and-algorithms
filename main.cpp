8a71e18bf3974e67c5d52c7bc1472d761dc00b61
ukpkmkk <657714340@qq.com>
2024-12-09 13:48:21 +0800

    linklist

--- main.cpp
+++ main.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
+
+#include "LinkList.cpp"
 #include "Seqlist.cpp"
+#include "ListNode.cpp"
 
 int main() {
     Seqlist<int> list(10);
+    ListNode<int> list=new ListNode<int>();
+
+
     return 0;
 }
